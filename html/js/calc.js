22d5d4da1a71e629d6f0ffb41f712188ba0ef0b1
Kouji Kozaki <kozaki@ei.sanken.osaka-u.ac.jp>
2018-10-21 13:32:12 +0900

    Update html/js/calc.js

--- html/js/calc.js
+++ html/js/calc.js
@@ -6,8 +6,10 @@ function print(str){
 
 document.write("<p>");
 
-var D = 10;
-var B = 8;
+var dist = { Moon:0.1, Mars:2, Jupiter:7, Saturn:10, Uranus:30, Neptune:50, Pluto:100, Centauri:1000000};
+
+var D = dist['Moon'];
+var B = 1;
 var P = (1-0.05*B) / Math.sqrt(Math.log(D)/Math.log(10)+2)  ;
 
 var result = 'false' ;
