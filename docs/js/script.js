058209fae6159730449613cfd10ba2852e3a73ca
Kouji Kozaki <kozaki@ei.sanken.osaka-u.ac.jp>
2018-10-21 13:50:04 +0900

    Pages用テスト

---
+++ docs/js/script.js
@@ -0,0 +1,30 @@
+var param = getParameter();
+
+$(function () {
+	$(".item").click(function(){
+		$(this).toggleClass("active");
+	});
+});
+
+//GETƒpƒ‰ƒ[ƒ^‚Ìˆ—
+function getParameter()
+{
+    var result = {};
+    if( 1 < window.location.search.length )
+    {
+        var query = window.location.search.substring( 1 );
+
+        var parameters = query.split( '&' );
+
+        for( var i = 0; i < parameters.length; i++ )
+        {
+            var element = parameters[ i ].split( '=' );
+
+            var paramName = decodeURIComponent( element[ 0 ] );
+            var paramValue = decodeURIComponent( element[ 1 ] );
+
+            result[ paramName ] = paramValue;
+        }
+    }
+    return result;
+}
