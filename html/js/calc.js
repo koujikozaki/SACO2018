66159ea06a78d86382aa49b8cc73ce95cb46fd33
Wakabayashi Kenichi <kwaka1208@croads.jp>
2018-10-21 13:57:16 +0900

    判定処理追加

--- html/js/calc.js
+++ html/js/calc.js
@@ -6,17 +6,19 @@ function print(str){
 
 document.write("<p>");
 
-var dist = { Moon:0.1, Mars:2, Jupiter:7, Saturn:10, Uranus:30, Neptune:50, Pluto:100, Centauri:1000000};
+function judge(count, star) {
+	var dist = { Moon:0.1, Mars:2, Jupiter:7, Saturn:10, Uranus:30, Neptune:50, Pluto:100, Centauri:1000000};
 
-var D = dist['Moon'];
-var B = 1;
-var P = (1-0.05*B) / Math.sqrt(Math.log(D)/Math.log(10)+2)  ;
+	var D = dist[star];
+	var B = count;
+	var P = (1-0.05*B) / Math.sqrt(Math.log(D)/Math.log(10)+2)  ;
 
-var result = 'false' ;
-if ( P > Math.random(1) ){
-	result  = 'true' ;
+	var result = false;
+	if ( P > Math.random(1) ){
+		result  = true;
+	}
+	return result;
 }
-
 
 print("P = " +  P);
 print("Result = " +  result);
